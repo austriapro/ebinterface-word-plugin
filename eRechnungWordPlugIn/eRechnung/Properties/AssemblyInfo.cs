2f75ceabb00ff97b05388115192205e57407bffa
Josef Bogad <jbogad@hotmail.com>
2017-05-31 11:50:15 +0200

    RELEASE 4.3.0.5

--- eRechnungWordPlugIn/eRechnung/Properties/AssemblyInfo.cs
+++ eRechnungWordPlugIn/eRechnung/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Security;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-[assembly: AssemblyVersion("4.3.0.4")]
+[assembly: AssemblyVersion("4.3.0.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
