ed73ca9cb3229e15840b7ac5b32a8015f004ba87
Josef Bogad <jbogad@hotmail.com>
2018-10-12 21:05:59 +0200

    Cleanup in Compile Configuration

--- eRechnungWordPlugIn/SimpleEventBroker/Properties/AssemblyInfo.cs
+++ eRechnungWordPlugIn/SimpleEventBroker/Properties/AssemblyInfo.cs
@@ -43,4 +43,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.2.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.1.0")]
--- eRechnungWordPlugIn/ebIModelsTests/Properties/AssemblyInfo.cs
+++ eRechnungWordPlugIn/ebIModelsTests/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [Assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.1.0")]
